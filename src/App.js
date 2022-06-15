821d55ee6b32d0f04fa27cb848c0824d6fa263e6
meraylayous <memimemi2019@gmail.com>
2022-06-15 19:21:25 +0300

    modifyed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//import Todo from './components/Todo';
+//import TodoForm from './components/TodoForm';
+//import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+    <h1>Todo app</h1>
+    <TodoList/>
+    </div>
+  );
+}
+
+export default App;
